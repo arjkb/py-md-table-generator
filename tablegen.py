ba66889bd986d01d9128c0326d49bbc276a3661d
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2017-12-02 23:08:37 -0700

    insert separator bars; spaces gets complicated, as there are spaces within the title, author itself

--- tablegen.py
+++ tablegen.py
@@ -1,5 +1,12 @@
 def main():
-    print("hi")
+    with open('input.txt') as f:
+        for i, line in enumerate(f, 1):
+            title, link, auth = line.split('|')
+
+            print(i)
+            print(title.strip())
+            print(link.strip())
+            print(auth.strip())
 
 if __name__ == '__main__':
     main()
