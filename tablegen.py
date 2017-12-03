ed177cdd82de1435cb2ba0afe7790b644d745719
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2017-12-02 23:15:55 -0700

    switch order of author and link

--- tablegen.py
+++ tablegen.py
@@ -5,12 +5,12 @@ def main():
 
     with open('input.txt') as f:
         for i, line in enumerate(f, 1):
-            title, link, auth = line.split('|')
+            title, auth, link = line.split('|')
 
             s = t.substitute(sno=i,
                             title=title.strip(),
                             link=link.strip(),
-                            author=auth.strip())
+                            author=auth.strip() )
             print(s)
 
 if __name__ == '__main__':
