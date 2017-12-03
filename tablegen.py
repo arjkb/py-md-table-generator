d670ee08a240c282b887599288c53e5ef7426685
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2017-12-02 23:14:41 -0700

    printing out table contents

--- tablegen.py
+++ tablegen.py
@@ -1,12 +1,17 @@
+from string import Template
+
 def main():
+    t = Template("|$sno|[$title]($link){:target='_blank'}|$author|")
+
     with open('input.txt') as f:
         for i, line in enumerate(f, 1):
             title, link, auth = line.split('|')
 
-            print(i)
-            print(title.strip())
-            print(link.strip())
-            print(auth.strip())
+            s = t.substitute(sno=i,
+                            title=title.strip(),
+                            link=link.strip(),
+                            author=auth.strip())
+            print(s)
 
 if __name__ == '__main__':
     main()
